5deb5944e0a02dff568e927d3b8e8a1d5bccb3e3
Joseph D. Gaeddert <joseph@liquidsdr.org>
2020-03-09 10:52:50 -0400

    adding functional class for generating signal/spectrum

--- support.js
+++ support.js
@@ -39,3 +39,69 @@ function svg_add_labels(_svg, _margin, _width, _height, _xlabel, _ylabel)
        .text(_ylabel)
 }
 
+// normal random number
+function randn()
+{
+    let u1 = 0, u2 = Math.random();
+    while (u1 == 0) { u1 = Math.random(); }
+    return Math.sqrt(-2*Math.log(u1)) * Math.sin(2*Math.PI*u2);
+}
+
+// function class to generate signals, power spectral density
+function siggen(nfft)
+{
+    this.nfft = nfft;
+    this.fft  = new FFTNayuki(nfft);
+    this.xi   = new Array(nfft);
+    this.xq   = new Array(nfft);
+    this.psd  = new Array(nfft);
+    this.m    = Math.min(120,Math.floor(0.4*nfft)); // filter semi-length
+
+    // clear internal buffer
+    this.clear = function() {
+        for (var i=0; i<this.nfft; i++) {
+            this.xi[i] = 0;
+            this.xq[i] = 0;
+        }
+    }
+
+    // generate signals in the time-domain buffer
+    this.add_signal = function(_fc, _bw, _gain) {
+        let gain = Math.pow(10,_gain/20);
+        for (var i=0; i<2*this.m+1; i++) {
+            let v = Math.max(0.0001,Math.abs(Math.PI*_bw*(i-this.m)));
+            let w = Math.cos(0.5*Math.PI*(i-this.m)/this.m)**4;
+            let h = Math.sin(v) / v * w;
+            this.xi[i] += gain * Math.cos(2*Math.PI*_fc*i) * h;
+            this.xq[i] += gain * Math.sin(2*Math.PI*_fc*i) * h;
+        }
+    }
+
+    // add noise to time-domain buffer
+    this.add_noise = function(noise_floor_dB) {
+        // compute noise standard deviation, compensating for fft size and I/Q components
+        let nstd = Math.pow(10.,noise_floor_dB/20) / Math.sqrt(2 * this.nfft);
+        for (var i=0; i<this.nfft; i++) {
+            this.xi[i] += randn() * nstd;
+            this.xq[i] += randn() * nstd;
+        }
+    }
+
+    // run fft, adding noise floor as appropriate
+    this.generate = function(noise_floor_dB) {
+        // compute noise floor (minimum prevents possibility of log of zero)
+        if (noise_floor_dB==null)
+            { noise_floor_dB = -120; }
+        let noise_floor = Math.min(1e-12, Math.pow(10.,noise_floor_dB/10));
+
+        // compute transform in place
+        this.fft.forward(this.xi,this.xq);
+
+        // convert to dB (adding noise floor as appropriate), and apply fft shift
+        for (var i=0; i<this.nfft; i++) {
+            let x2 = this.xi[i]**2 + this.xq[i]**2;
+            this.psd[(i+this.nfft/2)%nfft] = 10*Math.log10(noise_floor + x2);
+        }
+    }
+}
+
