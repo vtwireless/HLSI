77c2b6f9f616315a6e4758117554438035f03e40
Mayur Dhepe <mayurdhepe@gmail.com>
2023-04-24 12:53:32 -0400

    Updates to x3d scene and added Antenna pattern

--- spatialMobility.js
+++ spatialMobility.js
@@ -8,13 +8,13 @@ const BaseStation = class {
 
     // constructor for BaseStation 
     constructor(position, velocity, maxAcceleration, transmitters, receivers, txAntennas, RxAntennas) {
-      this.position = position;
-      this.velocity = velocity;
-      this.maxAcceleration = maxAcceleration;
-      this.transmitters = transmitters;
-      this.receivers = receivers;
-      this.txAntennas = txAntennas;
-      this.RxAntennas = RxAntennas;
+        this.position = position;
+        this.velocity = velocity;
+        this.maxAcceleration = maxAcceleration;
+        this.transmitters = transmitters;
+        this.receivers = receivers;
+        this.txAntennas = txAntennas;
+        this.RxAntennas = RxAntennas;
     }
 
     // Getters
@@ -32,62 +32,62 @@ const BaseStation = class {
 };
 
 // Transmitter
-const Transmitter = (Base) => 
+const Transmitter = (Base) =>
     class extends BaseStation {
 
-    // constructor for Transmitter 
-    constructor(signal) {
-        this.signal = signal;
-    }
+        // constructor for Transmitter 
+        constructor(signal) {
+            this.signal = signal;
+        }
 
-    // Getters
-    get getSignal() {
-        return this.signal;
-    }
+        // Getters
+        get getSignal() {
+            return this.signal;
+        }
 
-    // Setters
-    set setSignal(signal) {
-        this.signal = signal;
-    }
+        // Setters
+        set setSignal(signal) {
+            this.signal = signal;
+        }
 
-    // Instance methods
+        // Instance methods
 
-};
+    };
 
 // Receiver
-const Receiver = (Base) => 
+const Receiver = (Base) =>
     class extends BaseStation {
 
-    // constructor for Receiver 
-    /**
-         * @param {Signal} signal -- Signal of Interest
-         * @param {} signals -- List of all signals including interferers
-    */
-    constructor(signal, preSelectorFilter, iip3, fc_Rx, signals = []) {
-        this.signal = signal;
-    }
+        // constructor for Receiver 
+        /**
+             * @param {Signal} signal -- Signal of Interest
+             * @param {} signals -- List of all signals including interferers
+        */
+        constructor(signal, preSelectorFilter, iip3, fc_Rx, signals = []) {
+            this.signal = signal;
+        }
 
-    // Getters
-    get getSignal() {
-        return this.signal;
-    }
+        // Getters
+        get getSignal() {
+            return this.signal;
+        }
 
-    // Setters
-    set setSignal(signal) {
-        this.signal = signal;
-    }
+        // Setters
+        set setSignal(signal) {
+            this.signal = signal;
+        }
 
-    // Instance methods
+        // Instance methods
 
-};
+    };
 
 // Antenna
 const Antenna = class {
 
     // constructor for Antenna 
     constructor(gain, pointingAngle) {
-      this.gain = gain;
-      this.pointingAngle = pointingAngle;
+        this.gain = gain;
+        this.pointingAngle = pointingAngle;
     }
 
     // Getters
@@ -103,3 +103,17 @@ const Antenna = class {
     // Instance methods
 
 };
+
+// Position of an object in 3D space (x, y, z)
+const Position = class {
+
+    // constructor for Position 
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+};
+
+
