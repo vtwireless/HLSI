e7709270bf1cc8e28bf94f0dbd293c3e89a12be4
Jack4891 <95482991+Jack4891@users.noreply.github.com>
2025-08-05 11:08:43 -0400

    added BER plot and fixed freeze button

---
+++ BER_plot.js
@@ -0,0 +1,81 @@
+
+function BER_plot(){
+
+    let ebnoPoints = [
+                  { x: sig.gn, y: sig.ber },
+                 ];
+
+
+    document.getElementById("BER_plotParent").innerHTML = "";
+  
+      // set the dimensions and margins of the graph
+      const margin = {top: 10, right: 30, bottom: 30, left: 60},
+              width = 460 - margin.left - margin.right,
+              height = 400 - margin.top - margin.bottom;
+
+      // append the svg object to the body of the page
+      var svg = d3.select("#BER_plotParent")
+          .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+          .attr("transform", `translate(${margin.left}, ${margin.top})`);
+        svg.append("rect")
+        .attr("x",0)
+        .attr("y",0)
+        .attr("height", height )
+        .attr("width", width )
+        .style("fill", "EBEBEB")
+          
+      // Add X axis
+      var x = d3.scaleLinear()
+        .domain([0, 20])
+        .range([ -.001, width * 1.01 ]);
+      svg.append("g")
+        // sends the line to the bottom
+        .attr("transform", "translate(0," + height + ")")
+        // make grid lines
+        .call(d3.axisBottom(x).tickSize(-height*1).ticks(7))
+
+      // Add Y axis
+      var y = d3.scaleLog()
+        .domain([10**-6, 1])
+        .range([ height, 0]);
+      svg.append("g")
+        // make grid lines
+        .call(d3.axisLeft(y).tickSize(-width*1).ticks(7))
+        // stroke lines
+        svg.selectAll(".tick line").attr("stroke", "#FFFFFF")
+        svg.selectAll(".tick text").attr("fill", "#FFFFFF")          
+
+
+      setInterval(() => {
+      ebnoPoints = [
+                  { x: sig.gn, y: sig.BER },
+                 ];
+      
+        svg.append('g')
+          .selectAll("dot")
+          .data(ebnoPoints)
+          .enter()
+          .append("circle")
+            .attr("cx", function (d) { return x(d.x); } )
+            .attr("cy", function (d) { return y(d.y); } )
+            .attr("r", 1.5)
+            .style("fill","#1b9e77")
+      }, refreshRate);
+}
+  
+  // helper functions
+
+  
+  function randn_bm() {
+      let u = 0, v = 0;
+      while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+      while(v === 0) v = Math.random();
+      let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+      num = num / 10.0 + 0.5; // Translate to 0 -> 1
+      if (num > 1 || num < 0) return randn_bm() // resample between 0 and 1
+       num = num - 0.5; // added to center around 0
+      return num
+    }
--- constellationDiagram_base_old.js
+++ constellationDiagram_base_old.js
@@ -1,19 +1,5 @@
-// Declare global variables
-let sentMessages = 0;
-let messageErrors = 0;
-let bpskFlag = 0;
-let refreshRate = 1000;
-
-
-
-
-function constellationDiagram(top , left,constellationName, frozenFlag){
-  
-  setConstellationPosition(top, left, constellationName);
-
-  // document.getElementById("constellationParent").innerHTML = "";
-
-    document.getElementById(constellationName).innerHTML = "";
+function constellationDiagram(){
+    document.getElementById("constellationParent").innerHTML = "";
     let distance = 2/2;
     // delcared for 16 qam as signal is also declared as 16 qam
     let constellationTargets = [
@@ -31,8 +17,7 @@ function constellationDiagram(top , left,constellationName, frozenFlag){
                 height = 400 - margin.top - margin.bottom;
   
         // append the svg object to the body of the page
-        // var svg = d3.select("#constellationParent")
-        var svg = d3.select("#"+constellationName)
+        var svg = d3.select("#constellationParent")
             .append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
@@ -77,174 +62,38 @@ function constellationDiagram(top , left,constellationName, frozenFlag){
           .attr("cy", function (d) { return y(d.y); } )
           .attr("r", 1.5)
           .style("fill","#1b9e77")
-
-
-          // Add a button to the SVG
-      svg.append("foreignObject")
-        .attr("x", width - 60) // Position the button near the top-right corner
-        .attr("y", 10)
-        .attr("width", 80)
-        .attr("height", 30)
-        .append("xhtml:button")
-        .text("freeze")
-        .style("font-size", "12px")
-        .style("padding", "5px")
-        .style("cursor", "pointer")
-        .style("width", "60px") // Add this line for fixed width
-        .on("click", freeze);
-
-
-      // let freezeBox = svg.append("foreignObject")
-      //   .attr("x", width - 60)
-      //   .attr("y", 10)
-      //   .attr("width", 80)
-      //   .attr("height", 30);
-
-      // freezeBox.append("xhtml:button")
-      //   .text("freeze")
-      //   .style("font-size", "12px")
-      //   .style("padding", "5px")
-      //   .style("cursor", "pointer")
-      //   .on("click", freeze);
-    
-        // Create a container to display BER
-        let berDisplay = document.createElement("div");
-        berDisplay.id = "berDisplay";
-        berDisplay.style.marginTop = "10px";
-        berDisplay.style.fontSize = "14px";
-        berDisplay.textContent = "Bit Errr Rate: 0";
-        document.getElementById(constellationName).appendChild(berDisplay);
-
-        // document.getElementById("constellationParent").appendChild(berDisplay);
-
+  
+  
         // console.log(sig)
 
         // UNCOMMENT out to add a slider
-        // Slider(noise, 'gn', null, "noiseSlider");
-        // Label(sig, 'sinr', {
-        // prefix: "Signal-to-Noise Ratio: SNR = "
-        // }, "noiseSlider");
-      
-
-
-        let messageRate = 50;
-
-        div = document.getElementById("rateSlider")
-      // Check if the rate slider already exists
-      console.log(div.hasChildNodes() == false)
-      
-      console.log(document.getElementById("rateSlider").hasChildNodes() == false)
-      if (!(document.getElementById("rateSlider").hasChildNodes())) {
-        messageRateSlider = document.createElement("INPUT");
-        messageRateSlider.type = "range";
-        messageRateSlider.min = 1; 
-        messageRateSlider.max = 500;
-        messageRateSlider.step = 1;
-        messageRateSlider.value = 1;
-        messageRateSlider.id = "rateSlider"; // Assign an ID to the slider
-
-        let p = document.createElement("p");
-        p.appendChild(messageRateSlider);
-        document.getElementById("rateSlider").appendChild(p); // 
-
-        // below sets up the label on the left of the slider
-        let label = document.createElement("label");
-        label.setAttribute("for", messageRateSlider.id);
-        label.appendChild(document.createTextNode("Message Rate"));
-        messageRateSlider.parentNode.prepend(label);
-  
-        // below sets up the output value on the right of the slider
-        let output = document.createElement("output");
-        output.setAttribute("for", messageRateSlider.id);
-        messageRateSlider._output = output;
-        messageRateSlider.parentNode.append(output);
-      
-        // below tells CSS to make the slider look like the sliders we're used to
-        messageRateSlider.className = "slider_bw";
-  
-        // we will use floats on the output
-        var parseValue = parseFloat;
-  
-        messageRateSlider.oninput = function () {
-          // Set the varying parameter.  For example: sig.freq
-          // This calls the parameter setter.
-          messageRate = parseValue(messageRateSlider.value);
-        };
-        var outputUnitsCallback = function (val) {
-          return d3.format("d")(val) + " messages/sec";
-        };
-
-  
-        messageRateSlider.addEventListener("input", function () {
-          messageRate = messageRateSlider.value;
-          output.textContent = outputUnitsCallback(messageRate);
-        })
-
-        output.value = outputUnitsCallback(messageRate);
-      }
-      // console.log(document.getElementById("rateSlider").p)
+        Slider(noise, 'gn', null, "noiseSlider");
+        Label(sig, 'sinr', {
+        prefix: "Signal-to-Noise Ratio: SNR = "
+        }, "noiseSlider");
   
       // console.log(sig.sinr)
   
       // START OF LOOPING CODE
       let counter = 0;
-      messageRateSlider.value = messageRate
-
-      
       setInterval(() => {
-
-        if(!frozenFlag){
-        if (sig.mcs == 0) {
-          bpskFlag = 0;
-        } else {
-          bpskFlag = 1;
-        }
-
-
-        let scaleFactor = 0
-        if(sig.mcs>1 && sig.mcs <5 ){
-          // 4 QAM
-          scaleFactor = 2
-      } else if(sig.mcs <2){
-          // BPSK
-        bpskFlag = true;
-        scaleFactor = 2
-      } else if (sig.mcs >=5 && sig.mcs <7 ){
-          // 16 QAM
-          scaleFactor = .707
-      } else if (sig.mcs == 7){
-          // 32 QAM
-          scaleFactor = .408
-      } else if(sig.mcs ==8){
-          // 64 QAM
-          scaleFactor = .353
-      } else if (sig.mcs == 9){
-          // 128 QAM
-          scaleFactor = .289
-      } else if(sig.mcs >9) {
-          // 256 QAM
-          scaleFactor = .25
-      }
       // console.log("1 second has passed");
       // const ebno = math.log10(math.abs(sig.sinr))
-      const ebno = (10 ** (sig.sinr/20)) 
+      const ebno = (10 ** (sig.sinr/10)) 
       const noisePower = 1/ebno
       // const noisePower = .1
-      // console.log(ebno)
   
-      const variance = scaleFactor* Math.sqrt(noisePower)
-      // console.log(variance)
+      const variance = Math.sqrt(noisePower) 
 
 
     
-        
+
 
       // console.log(ebno);
         counter = counter + 1;
-        if (counter == 9){
+        if (counter == 24){
           counter = 0;
-          // d3.select("#constellationParent").selectAll("circle").remove();
-          d3.select("#"+constellationName).selectAll("circle").remove();
+          d3.select("#constellationParent").selectAll("circle").remove();
           svg.append('g')
           .selectAll("dot")
           .data(constellationTargets)
@@ -256,12 +105,11 @@ function constellationDiagram(top , left,constellationName, frozenFlag){
             .style("fill","#1b9e77")
           console.log("10 seconds has passed")
         }
-      for (let i = 0; i < messageRate; i++) {
-        sentMessages = sentMessages + 1;
+      for (let i = 0; i < 99; i++) {
         // creates a variable that can be edited if it goes out of bounds for x and y values
         let translatedTargets = constellationTargets.map(point => ({
-          x: point.x  +(2)*variance*randn_bm(),
-          y: point.y  +(2)*variance*randn_bm()
+          x: point.x + variance * randn_bm(),
+          y: point.y + variance * randn_bm()
         }));
         for (let i = 0; i < constellationTargets.length; i++) {
           // console.log(translatedTargets[i])
@@ -270,60 +118,15 @@ function constellationDiagram(top , left,constellationName, frozenFlag){
           let xErrorDist = Math.abs(translatedTargets[i].x - constellationTargets[i].x); 
           let yErrorDist = Math.abs(translatedTargets[i].y - constellationTargets[i].y); 
 
-          let freezeBoxX = .98;
-          let freezeBoxY = 1.18;
-          let freezeBoxWidth = .5;
-          let freezeBoxHeight = .25;
-
           let = printObject = [translatedTargets[i]]
 
           if ( (xErrorDist > distance) || (yErrorDist > distance) ){
-            messageErrors = messageErrors + 1;
-          }
-
-          // console.log(printObject[0].x)
-          if (  !((printObject[0].x >= freezeBoxX &&
-                printObject[0].x <= freezeBoxX + freezeBoxWidth) &&
-                (printObject[0].y >= freezeBoxY &&
-                printObject[0].y <= freezeBoxY + freezeBoxHeight))){
-                  // console.log("Not in freeze box")
-
-
-
-
-            if ( (xErrorDist > distance) || (yErrorDist > distance) ){
-              // console.log("Error!!!")
-              // messageErrors = messageErrors + 1;
-              svg.append('g')
-              .selectAll("dot")
-              .data(printObject)
-              .enter()
-              .append("circle")
-                .attr("cx", function (d) { 
-                  let xVal = d.x
-                  if (xVal> 1.5){
-                    xVal = 1.5 
-                  } else if(xVal < -1.5){
-                    xVal = -1.5
-                  }
-                  return x(xVal); } )
-                .attr("cy", function (d) {
-                  let yVal = d.y      
-                  if (yVal> 1.5){
-                    yVal = 1.5
-                  } else if (yVal < -1.5){
-                    yVal = -1.5
-                  }
-                  return y(yVal); } )
-                .attr("r", 2)
-                .style("fill","#d95f02")
-            } else {
-              
-              svg.append('g')
-              .selectAll("dot")
-              .data(printObject)
-              .enter()
-              .append("circle")
+            // console.log("I am called!")
+            svg.append('g')
+            .selectAll("dot")
+            .data(printObject)
+            .enter()
+            .append("circle")
               .attr("cx", function (d) { 
                 let xVal = d.x
                 if (xVal> 1.5){
@@ -340,23 +143,34 @@ function constellationDiagram(top , left,constellationName, frozenFlag){
                   yVal = -1.5
                 }
                 return y(yVal); } )
-                .attr("r", 2)
-                .style("fill","#7570b3")
+              .attr("r", 2)
+              .style("fill","#d95f02")
+          } else {
+            svg.append('g')
+            .selectAll("dot")
+            .data(printObject)
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) { 
+              let xVal = d.x
+              if (xVal> 1.5){
+                xVal = 1.5 
+              } else if(xVal < -1.5){
+                xVal = -1.5
               }
-            }
-            else{
-              // console.log("In freeze box")
-              // console.log(printObject)
-            }
-            
+              return x(xVal); } )
+            .attr("cy", function (d) {
+              let yVal = d.y      
+              if (yVal> 1.5){
+                yVal = 1.5
+              } else if (yVal < -1.5){
+                yVal = -1.5
+              }
+              return y(yVal); } )
+              .attr("r", 2)
+              .style("fill","#7570b3")
           }
-
-
-
-        // constellationTargets = [
-        //           { x: 6, y: .1 },
-
-        //       ];
+        }
 
         svg.append('g')
           .selectAll("dot")
@@ -368,20 +182,8 @@ function constellationDiagram(top , left,constellationName, frozenFlag){
             .attr("r", 1.5)
             .style("fill","#1b9e77")
             }
-
-        // console.log("Sent Messages: " + sentMessages)
-        // console.log("Message Errors: " + messageErrors)
-        // console.log("Error Rate: " + messageErrors/sentMessages)
         // value below is the loop time in miliseconds
-        sig.BER = messageErrors/sentMessages;
-        // console.log("Bit Error Rate: " + sig.BER);
-
-        // Update the BER display
-        berDisplay.textContent = `Bit Error Rate: ${sig.BER.toFixed(6)}`;
-          }
-
-
-    }, refreshRate);
+    }, 1000);
   
   
     sig.onChange("mcs", update_constellation);
@@ -419,8 +221,8 @@ function constellationDiagram(top , left,constellationName, frozenFlag){
     }
   
     // delete all dots
-    // d3.select("#constellationParent").selectAll("circle").remove();
-    d3.select("#"+constellationName).selectAll("circle").remove();
+    d3.select("#constellationParent").selectAll("circle").remove();
+  
     svg.append('g')
       .selectAll("dot")
       .data(constellationTargets)
@@ -430,17 +232,19 @@ function constellationDiagram(top , left,constellationName, frozenFlag){
         .attr("cy", function (d) { return y(d.y); } )
         .attr("r", 1.5)
         .style("fill","#1b9e77")
-    }
-    function freeze() {
-      frozenFlag = !frozenFlag;
-      console.log(frozenFlag);
-    
-      // Update the button text
-      const button = d3.select("#"+constellationName+" button");
-      button.text(frozenFlag ? "unfreeze" : "freeze");
+
+
+        // console.log("Sent Messages: " + sentMessages)
+        // console.log("Message Errors: " + messageErrors)
+        // console.log("Error Rate: " + messageErrors/sentMessages)
+        // value below is the loop time in miliseconds
+        sig.BER = messageErrors/sentMessages;
+        // console.log("Bit Error Rate: " + sig.BER);
+
+        // Update the BER display
+        berDisplay.textContent = `Bit Error Rate: ${sig.BER.toFixed(6)}`;
     }
 
-    
   }
   
   // helper functions
@@ -452,36 +256,7 @@ function constellationDiagram(top , left,constellationName, frozenFlag){
       while(v === 0) v = Math.random();
       let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
       num = num / 10.0 + 0.5; // Translate to 0 -> 1
-      // if (num > 1 || num < 0) return randn_bm() // resample between 0 and 1
+      if (num > 1 || num < 0) return randn_bm() // resample between 0 and 1
        num = num - 0.5; // added to center around 0
       return num
     }
-
-
-     // use the clear all circles button
-function buttonPressedClearPoints(){
-  // d3.select("#constellationParent").selectAll("circle").remove();
-  d3.select("#constellationParent").selectAll("circle").remove();
-  d3.select("#constellationParent2").selectAll("circle").remove();
-
-  sig.BER = 0;
-  sentMessages = 0;
-  messageErrors = 0;  
-
-}
-
-// function freeze() {
-//   frozenFlag = !frozenFlag;
-//   console.log(frozenFlag);
-
-//   // Update the button text
-//   const button = d3.select("#constellationParent button");
-//   button.text(frozen ? "unfreeze" : "freeze");
-// }
-
-function setConstellationPosition(top, left, parent) {
-  const constellationParent = document.getElementById(parent);
-  constellationParent.style.top = `${top}px`;
-  constellationParent.style.left = `${left}px`;
-}
-
