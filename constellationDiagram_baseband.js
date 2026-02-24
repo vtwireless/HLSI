d8974080a0de95f669cc4f3bee5ec7e025ef378d
Jack4891 <95482991+Jack4891@users.noreply.github.com>
2026-02-24 11:31:27 -0500

    unfixed power calculatoins
    I was wrong in my initial guess. The RMS voltage conversion is already used in the measurement of noise power.

--- constellationDiagram_baseband.js
+++ constellationDiagram_baseband.js
@@ -230,7 +230,7 @@ function constellationDiagram_Baseband(){
       let offset = 1;
       if(!sig.differentialMode) offset = .5;
       let rmsPower = (sig.gn*(1/Math.sqrt(2)))**2;
-      let ebno = (offset*(rmsPower )**2/((noise.gn**2)));
+      let ebno = (offset*(sig.gn )**2/((noise.gn**2)));
       let ebnoDb = 10*Math.log10((ebno));
       d3.select("#ebnoLabel").text(`${ebnoDb.toFixed(2)} dB`);
       d3.select("#snrLabel").text(`${ebnoDb.toFixed(2)} dB`);
